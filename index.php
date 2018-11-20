ee2e5b0b58ad398e94c74e0a494fb97a15d1e767
root <personne.nico@gmail.com>
2018-11-20 15:26:07 +0100

    edit

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
- <!DOCTYPE html>
- <html lang="fr" dir="ltr">
-   <head>
-     <meta charset="utf-8">
-     <title></title>
-   </head>
-   <body>
-     <form method="post">
-     <select name="check">
-       <option value="true">True</option>
-       <option value="false">False</option>
-     </select>
-     <input type="submit" name="button" value="Valider">
-     </form>
-     <?php
-     $isEasy = $_POST['check'];
-     if (isset($_POST['button'])){
-       if($isEasy == true){
-       echo "<p>c'est facile<p>";
-     } else {
-       echo "<p>c'est difficile<p>";
-     }
-   }
-      ?>
-   </body>
- </html>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+  <form method="post">
+    <select name="check">
+      <option value="true">True</option>
+      <option value="false">False</option>
+    </select>
+    <input type="submit" name="button" value="Valider">
+  </form>
+  <a href="http://PHP/"><button type="button" class="btn btn-danger" name="button">Accueil</button></a>
+  <?php
+  $isEasy = (isset($_POST['check']));
+  if (isset($_POST['button'])){
+    if($isEasy == 'true'){
+      echo 'c\'est facile';
+    } else {
+      echo 'c\'est difficile';
+    }
+  }
+  ?>
+</body>
+</html>
